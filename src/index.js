8e5ab26885f280b70a80b6bc09242e1aa96da7d9
孙海波 <1064446256@qq.com>
2024-01-03 14:39:19 +0800

    备份文件

--- src/index.js
+++ src/index.js
@@ -8,15 +8,15 @@ import * as ToolkitDevice from "./modules/device";
 import * as ToolkitTrowser from "./modules/browser";
 import * as ToolkitTime from "./modules/time";
 import * as ToolkitJs from "./modules/js";
-export default {
-    ToolkitNumber,
-    ToolkitArray,
-    ToolkitString,
-    ToolkitConversion,
-    ToolkitStorage,
-    ToolkitVerify,
-    ToolkitDevice,
-    ToolkitTrowser,
-    ToolkitTime,
-    ToolkitJs,
+
+// 当前时间
+export const nowTime = () => {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = now.getMonth();
+    const date = now.getDate() >= 10 ? now.getDate() : ('0' + now.getDate());
+    const hour = now.getHours() >= 10 ? now.getHours() : ('0' + now.getHours());
+    const miu = now.getMinutes() >= 10 ? now.getMinutes() : ('0' + now.getMinutes());
+    const sec = now.getSeconds() >= 10 ? now.getSeconds() : ('0' + now.getSeconds());
+    return +year + "年" + (month + 1) + "月" + date + "日 " + hour + ":" + miu + ":" + sec+"-------";
 };
